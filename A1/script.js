e6cc8fa4b742e4f4c5c76662b8d51a23b0504867
PavithraBalamurugan2026 <144635857+PavithraBalamurugan2026@users.noreply.github.com>
2026-02-23 03:54:59 -0800

    Update script.js

--- A1/script.js
+++ A1/script.js
@@ -1,17 +1,23 @@
-function calculateAge() {
+function calculateAge() 
+{
     const dobInput = document.getElementById('dob').value;
     const resultElement = document.getElementById('ageResult');
     if (!dobInput) return resultElement.textContent = "Please enter date of birth.";
-
     const dob = new Date(dobInput);
     const now = new Date();
     let y = now.getFullYear() - dob.getFullYear();
     let m = now.getMonth() - dob.getMonth();
     let d = now.getDate() - dob.getDate();
-
-    if (d < 0) { m--; d += new Date(now.getFullYear(), now.getMonth(), 0).getDate(); }
-    if (m < 0) { y--; m += 12; }
-    
+    if (d < 0) 
+    {
+        m--; d += new Date(now.getFullYear(), now.getMonth(), 0).getDate();
+    }
+    if (m < 0) 
+    {
+        y--; m += 12;
+    }
     if (dob > now) resultElement.textContent = "Invalid date.";
     else resultElement.textContent = `Age: ${y}y ${m}m ${d}d`;
 }
+
+
